2427bca4cbf558069b1d473bb664c563c98de80e
Ali <Alijasimuhammmed@gmail.com>
2023-04-15 02:48:43 +0300

    add screenutl

---
+++ lib/pages/welcome/welcome.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        width: 375.w,
+        child: Stack(children: []),
+      ),
+    );
+  }
+}
